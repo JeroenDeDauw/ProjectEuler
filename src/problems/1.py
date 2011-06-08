9bcc53c3844d8f2604e76e154c30e7403836ede8
Jeroen De Dauw <jeroendedauw@gmail.com>
2011-06-08 17:43:32 +0200

    added prblem 2

--- src/problems/1.py
+++ src/problems/1.py
@@ -1,11 +1,11 @@
+#!/usr/bin/python2.7
+
 '''
 Created on Jun 8, 2011
 
 @licence GNU GPL v3+
 @author: Jeroen De Dauw < jeroendedauw@gmail.com >
 '''
-
-import sys
 
 def isMultiple( bases, number ):
     for base in bases:
---
+++ src/problems/2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python2.7
+
+'''
+Created on Jun 8, 2011
+
+@licence GNU GPL v3+
+@author: Jeroen De Dauw < jeroendedauw@gmail.com >
+'''
+
+import math
+
+def getFiboSum( upperLimit ):
+    sum, prev, current = 0, 0, 1
+    
+    while current < upperLimit:
+        if current % 2 == 0:
+            sum += current
+        prev, current = current, prev + current
+    
+    return sum
+
+def main():
+    print getFiboSum( 4 * math.pow(10, 6) )
+
+if __name__ == '__main__':
+    main()
